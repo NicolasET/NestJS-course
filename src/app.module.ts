43f1abe34e66c8ea22b591520ba1dd2c48a36846
NicolasET <nicolaset98@gmail.com>
2022-09-30 22:00:46 -0500

    Save

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
 import { BrandsModule } from './brands/brands.module';
 import { SeedModule } from './seed/seed.module';
+import { TestingModule } from './testing/testing.module';
 
 @Module({
-  imports: [CarsModule, BrandsModule, SeedModule],
+  imports: [CarsModule, BrandsModule, SeedModule, TestingModule],
   controllers: [],
   providers: [],
   exports: [],
